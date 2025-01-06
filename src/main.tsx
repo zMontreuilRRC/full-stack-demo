65c4ad13369be0f64f87015f7eaf9f19b0ea7d54
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-01-06 10:02:43 -0600

    Clean starter code, add notes for initial setup'

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// creates the root React component of our app at the #root HTML element
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* renders the App component */}
     <App />
   </StrictMode>,
 )
