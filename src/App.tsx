5f1efb83bc3b0625912fe15e4b677725140dc25a
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-01-08 14:48:41 -0600

    Add static version of webpage in React -- still needs styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Nav, Footer} from "./common/common";
+import { Landing } from "./landing/landing"
+
 // functions that return JSX are React Components
 // files must have .tsx extension
 function App() {
   return (
     <>
-      <div>Hello, world!</div>
+      <Nav />
+      <Landing />
+      <Footer />
     </>
   )
 }
---
+++ src/common/common.tsx
@@ -0,0 +1,29 @@
+export function Footer() {
+    return(<footer>
+        Complexicon &#169; Me, 2025
+    </footer>);
+}
+
+export function Nav() {
+    return(<nav>
+         <div className="page-links">
+            <span>
+                <a href="#">Home</a>
+            </span>
+            <span>
+                <a href="#">Top Terms</a>
+            </span>
+            <span>
+                <a href="#">My Terms</a>
+            </span>
+            <span>
+                <a href="#">My Contexts</a>
+            </span>
+        </div>
+        <div className="user-management-links">
+            <span>
+                <a href="#">Log In</a>
+            </span>
+        </div>
+    </nav>);
+}
