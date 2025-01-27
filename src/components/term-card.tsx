bcd8258f44b10a0d24945528f3e8e1fd0d281ad4
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-01-27 12:51:24 -0600

    Add functionality for search, card expansion using internal data

--- src/components/term-card.tsx
+++ src/components/term-card.tsx
@@ -1,22 +1,23 @@
 import { Term } from "../interfaces/term";
 import { ToggleSaveButton } from "./common";
 
-export function TermCard({term, isExpanded, key, onTitleClick, } 
-    : {term: Term, isExpanded: boolean, key: number, onTitleClick: (id: number) => void}
+export function TermCard({term, isExpanded, onTitleClick, } 
+    : {term: Term, isExpanded: boolean, onTitleClick: (id: number) => void}
 ) {
     return (
     <div className="term-card">
         {/* clicking on one card's definition may close the defs for other cards */}
-        <h3 onClick={() => onTitleClick(key)}>
+        <h3 onClick={() => onTitleClick(term.id)}>
             {term.title}
             {/* TODO: onClick toggles save of term */}
-            <ToggleSaveButton onClick={() => null} isSaved={term.isFavourite}/>
+            <ToggleSaveButton 
+                onClick={() => null} 
+                isSaved={term.isFavourite}
+            />
         </h3>
-        { isExpanded ? (
-                <p>
-                    {term.definition}
-                </p>
-            ) : null
+        { isExpanded 
+            ? <p>{term.definition}</p>
+            : null
         }
     </div>);
 }
