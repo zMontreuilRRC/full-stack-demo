6c1e8f288f14dc7f65c110998cda3ceaabb407d2
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-03-14 11:03:21 -0500

    Add foundation of terms service/routes/controllers, temp terms

---
+++ apps/backend/src/api/v1/services/termService.ts
@@ -0,0 +1,51 @@
+import tempTerms from "./tempTerms";
+
+// it MAY be desirable to make this a shared definition
+// consider if a Term is identical/extensible between front/backend
+export type Term = {
+    id: number,
+    title: string,
+    definition: string,
+    isFavourite: boolean
+}
+
+export const fetchAllTerms = async(): Promise<Term[]> => {
+    return tempTerms;
+}
+
+export const createTerm = async(term: {
+    title: string,
+    definition: string
+}): Promise<Term> => {
+    const newTerm: Term = {
+        id: tempTerms.length + 1, 
+        isFavourite: false,
+        ...term
+    };
+
+    tempTerms.push(newTerm);
+    return newTerm;
+}
+
+export const updateTerm = async(
+    id: number,
+    term: {title: string, definition: string, isFavourite: boolean}
+): Promise<Term> => {
+    const index: number = tempTerms.findIndex(t => t.id === id);
+    if(index === -1){
+        throw new Error(`Term with id ${id} not found`);
+    }
+
+    tempTerms[index] = {id, ...term};
+    return tempTerms[index];
+}
+
+export const deleteTerm = async(id: number): Promise<void> => {
+    const index: number = tempTerms.findIndex(t => t.id === id);
+
+    if(index === -1){
+        throw new Error(`Term with id ${id} not found`);
+    }
+
+    tempTerms.splice(index, 1);
+}
