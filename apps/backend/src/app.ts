dc6e4631d83967e92484a8df2a8b17af6b62c727
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-03-14 10:03:04 -0500

    Install and add morgan logging

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,6 +1,10 @@
 import express, {Express} from "express";
+import morgan from "morgan";
 
 const app: Express = express();
+
+// add morgan middleware, combined format logs info about each HTTP request
+app.use(morgan("combined"));
 
 app.get("/", (_req, res) => {
     res.send("Got response from backend!");
