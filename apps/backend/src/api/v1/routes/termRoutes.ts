34b8b54e4915e63840a3d29339d0ed5fdf77f89c
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-03-16 11:21:24 -0500

    Add basic CRUD controllers, routes

--- apps/backend/src/api/v1/routes/termRoutes.ts
+++ apps/backend/src/api/v1/routes/termRoutes.ts
@@ -1,5 +1,11 @@
 import express, {Router} from "express";
+import * as termController from "../controllers/termController";
 
 const router: Router = express.Router();
+
+router.get("/", termController.getAllTerms);
+router.post("/", termController.createTerm);
+router.put("/:id", termController.updateTerm);
+router.delete("/:id", termController.deleteTerm);
 
 export default router;
