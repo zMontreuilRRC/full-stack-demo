3ee3fdbd4fcbcc28be155eaafbb17d47318346dd
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-04-04 10:43:35 -0500

    add notes to cors config

--- apps/backend/config/cors.ts
+++ apps/backend/config/cors.ts
@@ -1,6 +1,8 @@
 import { CorsOptions } from "cors";
 
+// configure the type of requests that CORS will allow to be made to the backend
 const corsOptions: CorsOptions = {
+    // throw an error if the request does not come from the list of allowed origins
     origin: function(origin, callback) {
         const allowedOrigins = [process.env.FRONTEND_URL];
 
@@ -10,6 +12,7 @@ const corsOptions: CorsOptions = {
             callback(new Error("Not allowed by CORS restriction"), false);
         }
     },
+    // allow specific headers, methods, and inclusion of credentials
     allowedHeaders:['Content-Type', 'Authorization'],
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
     credentials: true
