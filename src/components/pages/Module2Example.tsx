e14360fe0711d3e026ae516462906fd14705b01f
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-08-05 12:30:52 -0500

    Add notes to example page

--- src/components/pages/Module2Example.tsx
+++ src/components/pages/Module2Example.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 
 /**
- * This is a simple page designed to demonstrate how state and props are handled between components. It is not included with the broader application.
+ * This is a simple page designed to demonstrate how state and props are handled between components. 
+ * It is not included with the broader application.
  */
 
 function StateExamplePage() {
     /**
      * We want to easily update and render these number values as they change.
-     * We initialize them in state with a variable name, a setter method, and an initial value.
+     * We initialize them in state with a:
+     *  Variable Name (countA, countB) that lets them be used as an ordinary READ-ONLY variable.
+     *  Setter Method (setCountA, setCountB) that is the ONLY WAY a state variable should be modified.
+     *  and an initial value (0, 10).
+     * 
+     * State should be initialized in the "highest" component that uses it. 
+     * That way it can pass it down as props (to the buttons) and use it as needed (in the Total).
      */
   const [countA, setCountA] = useState(0);
   const [countB, setCountB] = useState(10);
@@ -35,16 +42,25 @@ function StateExamplePage() {
         count={countA}
 
         // callback invokes the state set method. 
+        // setter methods are the ONLY WAY to update a state variable
+        // They can use the current state as an argument ("prev" here)
         // set the new state to the same as the previous, but +1
         handleClick={() => setCountA(prev => prev + 1)}
       />
 
+          {/* Same component as the previous, but different props
+            Note that this displays countB and sets it in the handler prop
+          */}
       <CounterButton
         label="Decrement B"
         count={countB}
         handleClick={() => setCountB(prev => prev - 1)}
       />
 
+          {/* 
+            Render the total of countA and countB. 
+            When either state Setter Method is invoked, this total re-renders.   
+          */}
       <div>
         Total: {countA + countB}
       </div>
@@ -53,6 +69,7 @@ function StateExamplePage() {
   );
 }
 
+// Defining types for props like this makes it easier and cleaner for defining what types a component will accept as props in TS
 type CounterButtonProps = {
   label: string;
   count: number;
