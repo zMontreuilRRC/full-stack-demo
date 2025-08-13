ce0c13b3a00c3df0d7ce67e9b03ecb0f6f70ead4
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-08-13 15:08:02 -0500

    Add notes for prisma, termService

--- apps/backend/prisma/client.ts
+++ apps/backend/prisma/client.ts
@@ -1,5 +1,12 @@
 import {PrismaClient} from "@prisma/client";
 
+/**
+ * Initialize a single PrismaClient (connection to the database) when 
+ * the app first imports it.
+ * 
+ * This prevents multiple connections to the db from prisma
+ * (see https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/instantiate-prisma-client)
+ */
 let prisma = new PrismaClient();
 
 export default prisma;
--- apps/backend/src/api/v1/services/termService.ts
+++ apps/backend/src/api/v1/services/termService.ts
@@ -1,4 +1,6 @@
+// Use the term type defined in prisma/schema.prisma
 import { Term } from "@prisma/client";
+// initialize a prisma client if not already and use in queries here
 import prisma from "../../../../prisma/client";
 
 /**
@@ -6,13 +8,16 @@ import prisma from "../../../../prisma/client";
  * methods on the ORM, which will send queries to the database and respond
  * with data needed.
  * 
+ * More general info on Prisma: https://www.prisma.io/docs/orm/overview/prisma-in-your-stack/rest
  */
 export const fetchAllTerms = async(): Promise<Term[]> => {
+    // get all records in the term table
     return prisma.term.findMany();
 }
 
 export const getTermById = async(id: number): Promise<Term | null> => {
     try {
+        // get first record that match the "where" object key/value pairs
         const term = prisma.term.findUnique({
             where: {
                 id: id
@@ -33,6 +38,7 @@ export const createTerm = async(termData: {
     title: string,
     definition: string
 }): Promise<Term> => {
+    // create a new term with termData as its column values, except for isFavourite as false
     const newTerm: Term = await prisma.term.create({
         data: {
             isFavourite: false,
@@ -47,6 +53,7 @@ export const updateTerm = async(
     id: number,
     term: {title: string, definition: string, isFavourite: boolean}
 ): Promise<Term> => {
+    // find a term where the id matches the id parameter, and update with the term argument for values
     const updateTerm = await prisma.term.update({
         where: {
             id: id
@@ -59,6 +66,7 @@ export const updateTerm = async(
 }
 
 export const deleteTerm = async(id: number): Promise<void> => {
+    // delete the term that matches the where key/value pairs
     await prisma.term.delete({
         where: {
             id: id
