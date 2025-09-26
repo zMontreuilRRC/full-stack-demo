5a0a37a2e4f38aee4035865cfa336da8d1370ce2
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-09-26 11:18:47 -0500

    Remove root dir shell script, update front and backend

--- apps/backend/vercel-ignore-preview.sh
+++ apps/backend/vercel-ignore-preview.sh
@@ -1,7 +1,8 @@
 # Script for preventing Vercel from deploying outside of "main" and "develop" branches
-if ["$VERCEL_GIT_BRANCH" != "main" ] && ["$VERCEL_GIT_BRANCH" != "develop"]; then
+if [["$VERCEL_GIT_COMMIT_REF" != "main" && "$VERCEL_GIT_COMMIT_BRANCH" != "develop"]] ; then
     echo "Skipping build for branch $VERCEL_GIT_BRANCH"
-    exit 0
+    exit 0;
 else
-    exit 1
+    echo "Build can proceed"
+    exit 1;
 fi
--- vercel-ignore-preview.sh
+++
@@ -1,7 +0,0 @@
-# Script for preventing Vercel from deploying outside of "main" and "develop" branches
-if ["$VERCEL_GIT_BRANCH" != "main" ] && ["$VERCEL_GIT_BRANCH" != "develop"]; then
-    echo "Skipping build for branch $VERCEL_GIT_BRANCH"
-    exit 0
-else
-    exit 1
-fi
